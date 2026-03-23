98b179786b316b75fe673b6c4f5de3e26453a590
BlastPowa <97843706+BlastPowa@users.noreply.github.com>
2026-03-23 15:58:44 +0000

    fixed model code structure

--- myapp/models.py
+++ myapp/models.py
@@ -1,66 +1,79 @@
 from django.db import models
 
 class Salesperson(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    sales_id = models.AutoField(primary_key=True)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'tbl_salespeople'
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=20)
-    address = models.CharField(max_length=100)
-    city = models.CharField(max_length=50)
-    state_province = models.CharField(max_length=50)
-    country = models.CharField(max_length=50)
-    postal_code = models.CharField(max_length=20)
+    cust_id = models.AutoField(primary_key=True)
+    given_name = models.CharField(max_length=50)
+    family_name = models.CharField(max_length=50)
+    contact_phone = models.CharField(max_length=20)
+    street_address = models.CharField(max_length=100)
+    loc_city = models.CharField(max_length=50)
+    loc_region = models.CharField(max_length=50)
+    loc_country = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=20)
 
 class Car(models.Model):
-    serial_number = models.CharField(max_length=50)
-    make = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
-    color = models.CharField(max_length=20)
-    year = models.IntegerField()
-    is_new = models.BooleanField(default=True)
+    car_id = models.AutoField(primary_key=True)
+    vin_number = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50)
+    car_model = models.CharField(max_length=50)
+    paint_color = models.CharField(max_length=20)
+    mfg_year = models.IntegerField()
+    for_sale = models.BooleanField(default=True)
 
 class SalesInvoice(models.Model):
-    invoice_number = models.CharField(max_length=50)
-    date = models.DateField()
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    salesperson = models.ForeignKey(Salesperson, on_delete=models.CASCADE)
+    inv_id = models.AutoField(primary_key=True)
+    inv_number = models.CharField(max_length=50)
+    sale_date = models.DateField()
+    vehicle = models.ForeignKey(Car, on_delete=models.CASCADE)
+    buyer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    seller = models.ForeignKey(Salesperson, on_delete=models.CASCADE)
 
 class Mechanic(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    mech_id = models.AutoField(primary_key=True)
+    mech_fname = models.CharField(max_length=50)
+    mech_lname = models.CharField(max_length=50)
 
 class ServiceTicket(models.Model):
-    service_ticket_number = models.CharField(max_length=50)
-    date_received = models.DateField()
-    date_returned = models.DateField(null=True, blank=True)
-    comments = models.TextField()
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    ticket_id = models.AutoField(primary_key=True)
+    ticket_ref = models.CharField(max_length=50)
+    received_on = models.DateField()
+    returned_on = models.DateField(null=True, blank=True)
+    staff_notes = models.TextField()
+    vehicle = models.ForeignKey(Car, on_delete=models.CASCADE)
+    client = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
 class Service(models.Model):
-    service_name = models.CharField(max_length=100)
-    hourly_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    svc_id = models.AutoField(primary_key=True)
+    svc_name = models.CharField(max_length=100)
+    rate_per_hour = models.DecimalField(max_digits=10, decimal_places=2)
 
 class Parts(models.Model):
-    part_number = models.CharField(max_length=50)
-    description = models.CharField(max_length=255)
-    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
-    retail_price = models.DecimalField(max_digits=10, decimal_places=2)
+    part_id = models.AutoField(primary_key=True)
+    sku_number = models.CharField(max_length=50)
+    part_desc = models.CharField(max_length=255)
+    cost_price = models.DecimalField(max_digits=10, decimal_places=2)
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2)
 
 class PartsUsed(models.Model):
-    part = models.ForeignKey(Parts, on_delete=models.CASCADE)
-    service_ticket = models.ForeignKey(ServiceTicket, on_delete=models.CASCADE)
-    number_used = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    usage_id = models.AutoField(primary_key=True)
+    item = models.ForeignKey(Parts, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(ServiceTicket, on_delete=models.CASCADE)
+    qty_used = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
 
 class ServiceMechanic(models.Model):
-    service_ticket = models.ForeignKey(ServiceTicket, on_delete=models.CASCADE)
-    service = models.ForeignKey(Service, on_delete=models.CASCADE)
-    mechanic = models.ForeignKey(Mechanic, on_delete=models.CASCADE)
-    hours = models.DecimalField(max_digits=5, decimal_places=2)
-    comment = models.TextField()
-    rate = models.DecimalField(max_digits=10, decimal_places=2)
+    assignment_id = models.AutoField(primary_key=True)
+    ticket = models.ForeignKey(ServiceTicket, on_delete=models.CASCADE)
+    action = models.ForeignKey(Service, on_delete=models.CASCADE)
+    worker = models.ForeignKey(Mechanic, on_delete=models.CASCADE)
+    labor_hours = models.DecimalField(max_digits=5, decimal_places=2)
+    work_comment = models.TextField()
+    billing_rate = models.DecimalField(max_digits=10, decimal_places=2)
