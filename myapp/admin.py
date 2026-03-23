d63567934b7322f12fea951c4b5c99fc344b5233
BlastPowa <97843706+BlastPowa@users.noreply.github.com>
2026-03-23 16:00:38 +0000

    removed random

--- myapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Salesperson, Customer, Car, SalesInvoice, Mechanic, ServiceTicket, Service, Parts, PartsUsed, ServiceMechanic
-
-# This loop registers every model at once
-models = [Salesperson, Customer, Car, SalesInvoice, Mechanic, ServiceTicket, Service, Parts, PartsUsed, ServiceMechanic]
-for model in models:
-    admin.site.register(model)
